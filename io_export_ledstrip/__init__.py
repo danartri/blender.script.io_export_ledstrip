d2781fb26fcca7bb3aeceed8417f5cf609aa8c09
0rel <me@0rel.com>
2015-05-10 21:32:49 +0200

    first experimental version...

--- io_export_ledstrip/__init__.py
+++ io_export_ledstrip/__init__.py
@@ -17,37 +17,108 @@
 #  ***** GPL LICENSE BLOCK *****
 
 bl_info = {
-    "name": "Export SVG Format (.svg)",
-    "author": "Martin Froehlich (maybites.ch) + Matthew Ready (craxic.com)",
-    "version": (0, 0, 6),
-    "blender": (2, 7, 0),
-    "location": "File > Export > Inkscape (.svg)",
-    "description": "The script exports Blender BezierCurves to SVG format.",
-    "warning": "Quick and dirty hack, no success guaranteed.",
-    "wiki_url": "http://wiki.blender.org/index.php/Extensions:2.6/Py/Scripts/Import-Export/Inkscape_SVG_Exporter",
-    "tracker_url": "",
-    "category": "Import-Export"
+	"name": "Export LED strip (.xml)",
+	"author": "Martin Froehlich (maybites.ch) & Aurelio Lucchesi (0rel.com)",
+	"version": (0, 0, 1),
+	"blender": (2, 7, 0),
+	"location": "File > Export > ledstrip (.xml)",
+	"description": "The script exports Blender curves to custom XML.",
+	"warning": "Quick and dirty hack, no success guaranteed.",
+	"wiki_url": "",
+	"tracker_url": "",
+	"category": "Import-Export"
 }
 
 # Ensure that we reload our dependencies if we ourselves are reloaded by Blender
 if "bpy" in locals():
-    import imp;
-    if "exporter" in locals():
-        imp.reload(exporter);
-        
-import bpy;
-from .exporter import Exporter;
+	import imp;
+	if "exporter" in locals():
+		imp.reload(exporter);
+
+
+import bpy
+from .exporter import Exporter
+
+from bpy.props import BoolProperty
+from bpy.props import EnumProperty
+from bpy.props import StringProperty
+from bpy.props import IntProperty
+
+
+class ExportLedstrip(bpy.types.Operator):
+	"""Export selection to ledstrip"""
+	
+	bl_idname = "ledstrip.xml"
+	bl_label = "ledstrip XML exporter"
+	bl_options = {'PRESET'}
+	filename_ext = ".xml"
+	
+	filepath = StringProperty( subtype='FILE_PATH' )
+	
+	
+	# Export options
+	
+	Verbose = BoolProperty(
+		name="Verbose",
+		description="Run the exporter in debug mode. Check the console for output",
+		default=False )
+	
+	CoordinateSystem = EnumProperty(
+		name="Coordinate System",
+		description="Use the selected coordinate system for export",
+		items=(('LEFT_HANDED', "Left-Handed", "Use a Y up, Z forward system or a Z up, -Y forward system"),
+			('RIGHT_HANDED', "Right-Handed", "Use a Y up, -Z forward system or a Z up, Y forward system")),
+		default='LEFT_HANDED' )
+	
+	Resolution = IntProperty(
+		name = 'Resolution',
+		description = 'Resolution of mesh',
+		default = 4,
+		min = 1, max = 64 )
+	
+	
+	def execute(self, context):
+		
+		self.filepath = bpy.path.ensure_ext(self.filepath, '.xml')
+	
+		from . import exporter
+		Exporter = exporter.Exporter(self, context)
+		Exporter.execute()
+		return {'FINISHED'}
+	
+	def invoke(self, context, event):
+		
+		# Check currently selected objects
+		curve_ok = False
+		for obj in bpy.context.selected_objects:
+			if obj.type == 'CURVE':
+				curve_ok = True
+				break
+		if len(bpy.context.selected_objects) == 0 or not curve_ok:
+			raise NameError( 'Please select at least one curve object!' )
+			return {'CANCELLED '}
+		
+		if not self.filepath:
+			self.filepath = bpy.path.ensure_ext( bpy.data.filepath, ".xml" )
+		context.window_manager.fileselect_add( self )
+		return {'RUNNING_MODAL'}
+
+
+
+
+
+
 
 def menu_func(self, context):
-    self.layout.operator(Exporter.bl_idname, text="Inkscape (.svg)");
+	self.layout.operator(ExportLedstrip.bl_idname, text="ledstrip (.xml)");
 
 def register():
-    bpy.utils.register_module(__name__);
-    bpy.types.INFO_MT_file_export.append(menu_func);
-    
+	bpy.utils.register_module(__name__);
+	bpy.types.INFO_MT_file_export.append(menu_func);
+
 def unregister():
-    bpy.utils.unregister_module(__name__);
-    bpy.types.INFO_MT_file_export.remove(menu_func);
+	bpy.utils.unregister_module(__name__);
+	bpy.types.INFO_MT_file_export.remove(menu_func);
 
 if __name__ == "__main__":
-    register()
+	register()
--- io_export_ledstrip/exporter.py
+++ io_export_ledstrip/exporter.py
@@ -1,162 +1,112 @@
-# --------------------------------------------------------------------------
-# Copyright (C) 2015: Martin Froehlich (maybites.ch), Matthew Ready (craxic.com)
-#
-# based on code from the defunct export script by jm soler, jmsoler_at_free.fr
-#
-# This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License
-# as published by the Free Software Foundation; either version 2
-# of the License, or (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software Foundation,
-# --------------------------------------------------------------------------
-
 import bpy
-from bpy_extras.io_utils import ExportHelper
-from bpy.types import BezierSplinePoint
-
-XML_HEADER = """<?xml version="1.0" standalone="no"?>
-<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 20010904//EN" "http://www.w3.org/TR/2001/REC-SVG-20010904/DTD/svg10.dtd">
-<svg width="{0}mm" height="{1}mm"  viewBox="0 0 {0} {1}" xmlns="http://www.w3.org/2000/svg">
-    <title>Bezier Curve : {2}</title>
-    <desc>This is an exported Bezier xml_path from Blender, using the ExportScript by maybites.ch and Craxic</desc>
-"""
-XML_PATH = """<path style="fill:none;stroke:#000000;stroke-width:1px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1" d="{}"/>
-"""
-XML_END = "</svg>"
-
-MOVE_COMMAND = 'M{},{} '
-LINE_COMMAND = 'L{},{} '
-CURVE_COMMAND = 'C{},{} {},{} {},{} '
-JOIN_COMMAND = 'Z '
 
 
-class CoordinateContext:
-    def __init__(self, min_x, min_y, max_x, max_y):
-        self.min_x = min_x
-        self.min_y = min_y
-        self.max_x = max_x
-        self.max_y = max_y
-
-    def cx(self, x):
-        return x - self.min_x
-
-    def cy(self, y):
-        return self.max_y - y
-
-
-def make_curve_command(previous, point, ctx, scale, translation):
-    return CURVE_COMMAND.format(
-        ctx.cx(previous.handle_right[0] * scale[0] + translation[0]),
-        ctx.cy(previous.handle_right[1] * scale[1] + translation[1]),
-        ctx.cx(point.handle_left[0] * scale[0] + translation[0]),
-        ctx.cy(point.handle_left[1] * scale[1] + translation[1]),
-        ctx.cx(point.co[0] * scale[0] + translation[0]),
-        ctx.cy(point.co[1] * scale[1] + translation[1]))
-
-
-def point_str(point, scale, translation):
-    if isinstance(point, BezierSplinePoint):
-        return "[x: {}, y: {}, z: {},\n" \
-               " x: {}, y: {}, z: {},\n" \
-               " x: {}, y: {}, z: {}]".format(point.handle_left[0] * scale[0] + translation[0],
-                                              point.handle_left[1] * scale[1] + translation[1],
-                                              point.handle_left[2] * scale[2] + translation[2],
-                                              point.co[0] * scale[0] + translation[0],
-                                              point.co[1] * scale[1] + translation[1],
-                                              point.co[2] * scale[2] + translation[2],
-                                              point.handle_right[0] * scale[0] + translation[0],
-                                              point.handle_right[1] * scale[1] + translation[1],
-                                              point.handle_right[2] * scale[2] + translation[2])
-    else:
-        return "[x: {}, y: {}, z: {}]".format(point.co[0] * scale[0] + translation[0],
-                                              point.co[1] * scale[1] + translation[1],
-                                              point.co[2] * scale[2] + translation[2])
-
-
-class Exporter(bpy.types.Operator, ExportHelper):
-    bl_idname = "export_svg_format.svg"
-    bl_label = "Inkscape SVG Exporter"
-    bl_options = {'PRESET'}
-    filename_ext = ".svg"
-
-    def execute(self, context):
-        # Ensure Blender is currently in OBJECT mode to allow data access.
-        bpy.ops.object.mode_set(mode='OBJECT')
-
-        # Set the default return state to FINISHED
-        result = {'FINISHED'}
-
-        # Check that the currently selected object contains mesh data for
-        # exporting
-        curve = bpy.context.selected_objects[0]
-        if not curve or curve.type != 'CURVE':
-            raise NameError("Cannot export: object %s is not a curve" % curve)
-
-		# this scales from blender to svg (blender 1.0 = 1m while svg 1.0 = 1mm)
-        scale = [1000, 1000, 1000]; #curve.dimensions * 1000;
-
-        # bound_box[0] == [left, bottom, down]
-        # bound_box[7] == [right, top, up]
-        # bound_box[i] == [x, y, z], 0 <= i <= 7
-        min_x = curve.bound_box[0][0]
-        max_x = curve.bound_box[7][0]
-        min_y = curve.bound_box[0][1]
-        max_y = curve.bound_box[7][1]
-
-        ctx = CoordinateContext(min_x, min_y, max_x, max_y)
-
-		# this calculates the documents size
-        width = (max_x - min_x) * scale[0]
-        height = (max_y - min_y) * scale[1]
-
-		# this centers the object inside the document
-        translation = [- scale[0] * curve.bound_box[0][0], scale[1] * curve.bound_box[0][1], scale[2] * curve.bound_box[0][2]]
-
-        paths = []
-        for spline in curve.data.splines:
-            path = ""
-            if spline.type == 'BEZIER':
-                print("Exporting BEZIER curve.")
-                first_curve_point = None
-                previous = None
-                for n, point in enumerate(spline.bezier_points):
-                    print("Point: " + point_str(point, scale, translation))
-
-                    if n == 0:
-                        first_curve_point = point
-                        path += MOVE_COMMAND.format(ctx.cx(point.co[0] * scale[0] + translation[0]),
-                                                    ctx.cy(point.co[1] * scale[1] + translation[1]))
-                    else:
-                        path += make_curve_command(previous, point, ctx, scale, translation)
-                    previous = point
-
-                if spline.use_cyclic_u == 1:
-                    path += make_curve_command(previous, first_curve_point, ctx, scale, translation)
-                    path += JOIN_COMMAND
-            elif spline.type == 'POLY':
-                print("Exporting POLY curve.")
-                for n, point in enumerate(spline.points):
-                    command = MOVE_COMMAND if n == 0 else LINE_COMMAND
-                    path += command.format(ctx.cx(point.co[0] * scale[0] + translation[0]),
-                                           ctx.cy(point.co[1] * scale[1] + translation[1]))
-                    print("Point: " + point_str(point, scale, translation))
-
-                if spline.use_cyclic_u == 1:
-                    path += JOIN_COMMAND
-            paths.append(path)
-
-        # Open the file for writing
-        with open(self.filepath, 'w') as f:
-            f.write(XML_HEADER.format(width, height, curve.name))
-            for path in paths:
-                f.write(XML_PATH.format(path))
-            f.write(XML_END)
-
-        return result
+class Exporter:
+	
+	def __init__(self, Config, context):
+		
+		self.Config = Config
+		self.context = context
+		
+		self.log( "Begin verbose logging\n" )
+	
+	def execute(self):
+		
+		ledstripXML = ''
+		selections = bpy.context.selected_objects
+		scn = bpy.context.scene
+		active_obj = scn.objects.active
+		
+		# ensure Blender is currently in OBJECT mode to allow data access.
+		bpy.ops.object.mode_set(mode = 'OBJECT')
+		
+		for obj in selections: # for first selected object
+			for group in obj.users_group: # All groups
+				for obj in group.objects:
+					
+					if( obj.type == 'CURVE' ):
+						
+						self.log( 'converting curve %s' % obj.name )
+						self.log( 'location: %s' % obj.location )
+						
+						ledstripXML += '\t<segment name="{}">\n'.format( obj.name )
+						
+						#for spline in obj.data.splines:
+						#	self.log( 'number of bezier points: ', len( spline.bezier_points ) )
+						#	for point in spline.bezier_points:
+						#		self.log( 'coord: ', point.co )
+						#		self.log( 'left handle: ', point.handle_left )
+						#		self.log( 'right handle: ', point.handle_right )
+						#		
+						#		frmt = '\t<coord x="{:.2f}" y="{:.2f}" z="{:.2f}"></coord>\n'
+						#		ledstripXML += frmt.format( point.co.x, point.co.y, point.co.z )
+						
+						
+						# create mesh out of curve
+						
+						bpy.ops.object.select_all(action='DESELECT') 
+						scn.objects.active = obj
+						obj.select = True
+						
+						def0 = obj.data.resolution_u
+						def1 = obj.data.fill_mode
+						def2 = obj.data.bevel_resolution
+						def3 = obj.data.bevel_depth
+						
+						obj.data.fill_mode = 'FULL'
+						obj.data.resolution_u = self.Config.Resolution
+						obj.data.bevel_resolution = 1 #resolution
+						obj.data.bevel_depth = 0.0 #thickness
+						bpy.ops.object.convert( target='MESH', keep_original=True )
+						
+						bpy.ops.group.objects_remove_all()
+						
+						obj.data.resolution_u = def0
+						obj.data.fill_mode = def1           #reverting
+						obj.data.bevel_resolution = def2
+						obj.data.bevel_depth = def3
+						
+						
+						
+						# dump(obj.data)
+						
+						newObj = scn.objects.active
+						mesh = newObj.data
+						self.log( 'number of vertices=%d' % len(mesh.vertices) )
+						for vert in mesh.vertices:
+							self.log( 'v %f %f %f' % (vert.co.x, vert.co.y, vert.co.z) )
+							frmt = '\t\t<coord x="{:.2f}" y="{:.2f}" z="{:.2f}"></coord>\n'
+							ledstripXML += frmt.format( vert.co.x, vert.co.y, vert.co.z )
+						
+						#self.log( 'number of faces=%d' % len(mesh.polygons) )
+						#for face in mesh.polygons:
+						#	self.log('face')
+						#	for vert in face.vertices:
+						#		self.log(vert)
+						
+						
+						# delete temporary mesh object
+						bpy.ops.object.delete( use_global=False )
+						
+						ledstripXML += '\t</segment>\n'
+		
+		
+		# restore previous selection
+		bpy.ops.object.select_all(action='DESELECT')
+		for obj in selections:
+			obj.select = True
+		scn.objects.active = active_obj
+		
+		# open the file and export XML
+		with open( self.Config.filepath, 'w' ) as f: # self.filepath, 'w' ) as f:
+			f.write( '<ledstrip>\n' )
+			f.write( ledstripXML )
+			f.write( '</ledstrip>\n' )
+		
+		self.log("ledstrip exported (%s)" % self.Config.filepath, MessageVerbose=True )
+		
+		return True
+	
+	def log( self, String, MessageVerbose=False ):
+		if self.Config.Verbose is True or MessageVerbose == True:
+			print( String )
